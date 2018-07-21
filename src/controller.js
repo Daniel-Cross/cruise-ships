71d9d223a6331c720c4102f29d8f0b73a41b0c74
Daniel-Cross <crossy1686@gmail.com>
2018-07-21 18:44:09 +0100

    rendered ports

--- src/controller.js
+++ src/controller.js
@@ -21,10 +21,27 @@
           backgroundIndex = 0;
         }
       }, 1000);
-
     },
 
+    renderPorts(ports) {
+      const portsElement = document.querySelector('#ports');
+      portsElement.style.width = '0px';
+
+      ports.forEach((port, index) => {
+        const newPortElement = document.createElement('div');
+        newPortElement.className = 'port';
+        newPortElement.dataset.portIndex = index;
+
+        portsElement.appendChild(newPortElement);
+
+        const portsElementWidth = parseInt(portsElement.style.width, 10);
+        portsElement.style.width = `${portsElementWidth + 256}px`;
+
+      });
+    },
   };
+
+
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Controller;
   } else {
